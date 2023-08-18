6cd8608d0a3401e3b4603f7fb09a682c5103319f
João Vítor <Joo Vtor@DESKTOP-1O6R9P5>
2023-08-18 00:18:50 -0300

    Adicionar arquivos de projeto.

---
+++ Controle/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region switch - case
+            int escolha = 2;
+             
+            switch (escolha)
+            {
+                default:
+                    Console.WriteLine("Opção Default");
+                    break;
+                case 1:
+                    Console.WriteLine("Opção 1:");
+                    break;
+                case 2:
+                    Console.WriteLine("Opção 2:");
+                    break;
+                case 3:
+                    Console.WriteLine("Opção 3:");
+                    break;
+            }
+        #endregion
+
+            #region Declaração Goto
+        Inicio:
+            Console.WriteLine("Escolha uma Opção");
+            int op = int.Parse(Console.ReadLine());
+            int valor = 0;
+
+            switch (op) 
+            {
+                default:
+                    goto Inicio;
+                    break;
+                case 1:
+                    valor += 100;
+                    break;
+                case 2:
+                    valor += 50;
+                    goto case 1;
+            }
+
+            Console.WriteLine("Valor final " + valor);
+            #endregion
+
+
+            Console.ReadKey();  
+
+        }
+    }
+}
